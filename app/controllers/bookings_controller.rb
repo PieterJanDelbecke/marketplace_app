551636142151bd44f482c50f0f7d5eedb283ec73
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-11-17 08:32:52 +1100

    some changes on the items new form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,28 @@
 class BookingsController < ApplicationController
     before_action :authenticate_user!
+    before-action :set_item
 
     def index
     end
+
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        user = current_user
+        @booking = @item.add_user(user)
+        redirect_to root_path
+    end
+
+    
+    
+    
+    private 
+    
+    def bookings_params
+        params.require(:booking).permit(:item_id)
+    end
+
+
 end
