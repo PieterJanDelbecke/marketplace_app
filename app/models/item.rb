6fc13dbcf2cc2b390bcb21f95cd11e93da07e1ee
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-11-18 13:31:05 +1100

    changed the signup page layout

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,12 @@ class Item < ApplicationRecord
 
   has_one_attached :picture
 
+  validates :name ,:description, :price, presence: true
+
+  validates :name, length: { minimum: 10, too_short: "%{count} characters is the minimum required." } 
+  validates :description , length: { maximum: 10, too_long: "%{count} characters is the maximum." }
+  validates :price, numericality: {only_integer: true}
+
   # enum new: { brand_new: 1, second_hand: 0}
 
 end
