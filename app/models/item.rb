6b9ea9cc4ff8bf3f4b5aa1d8a54ab5145eabb06a
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-11-19 15:07:37 +1100

    implemented styling on Items index page

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
 
   has_one_attached :picture
 
-  validates :name ,:description, :price, presence: true
+  validates :name ,:description, :price, :picture, presence: true
 
-  validates :name, length: { minimum: 10, too_short: "%{count} characters is the minimum required." } 
-  validates :description , length: { maximum: 10, too_long: "%{count} characters is the maximum." }
-  validates :price, numericality: {only_integer: true}
+  # validates :name, length: { minimum: 10, too_short: "%{count} characters is the minimum required." } 
+  # validates :description , length: { maximum: 10, too_long: "%{count} characters is the maximum." }
+  # validates :price, numericality: {only_integer: true}
 
   # enum new: { brand_new: 1, second_hand: 0}
 
